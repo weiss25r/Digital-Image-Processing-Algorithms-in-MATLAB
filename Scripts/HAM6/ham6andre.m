a268c636c9f6d0a1f374313f10f6d71822b61cf6
Raffaele Terracino <raffaeleterracino001@gmail.com>
2022-11-18 16:30:42 +0100

    more ham6

--- Scripts/HAM6/ham6andre.m
+++ Scripts/HAM6/ham6andre.m
@@ -1,13 +1,13 @@
-function out = ham6(img)
-    img4096 = round(img/17)*17;                                             %Trasforma l'immagina RGB in 4096 colori
+function [out, map16] = ham6(img)
+    img4096 = round(img/17);                                             %Trasforma l'immagina RGB in 4096 colori
 
-    [ind16,map16] = rgb2ind(img4096,16);                                    %'nodither'
+    [ind16,map16] = rgb2ind(img4096*17,16, 'nodither');                                    %'nodither'
 
     figure;imshow(ind16,map16);title('ind16 = quantized + dithering');
 
     out = zeros(size(img,1),size(img,2),'uint8');
     out(1,1) = ind16(1,1);
-    [h,w,z] = size(img);
+    [h,w,~] = size(img);
 
     x = ind16(1,1);
     lastpix = [map16(x+1,1) map16(x+1,2) map16(x+1,3)];
@@ -27,18 +27,18 @@ function out = ham6(img)
             err = min([d rd gd bd]);
 
             switch(err)
-                case 1
+                case d
                     out(i,j) = ind16(i,j);
                     lastpix = [map16(x+1,1) map16(x+1,2) map16(x+1,3)];
-                case 2
-                    out(i,j) = 16 + rgb4096(i,j,1);
-                    lastpix = [rgb4096(i,j,1), lastpix(2), lastpix(3)];
-                case 3
-                    out(i,j) = 32 + rgb4096(i,j,2);
-                    lastpix = [lastpix(1), rgb4096(i,j,2), lastpix(3)];
-                case 4
-                    out(i,j) = 48 + rgb4096(i,j,3);
-                    lastpix = [lastpix(1), lastpix(2), rgb4096(i,j,3)];
+                case rd
+                    out(i,j) = 16 + img4096(i,j,1);
+                    lastpix = [img4096(i,j,1), lastpix(2), lastpix(3)];
+                case gd
+                    out(i,j) = 32 + img4096(i,j,2);
+                    lastpix = [lastpix(1), img4096(i,j,2), lastpix(3)];
+                case bd
+                    out(i,j) = 48 + img4096(i,j,3);
+                    lastpix = [lastpix(1), lastpix(2), img4096(i,j,3)];
             end
 
             lastpix=[map16(x+1,1), map16(x+1,2), map16(x+1,3)];
