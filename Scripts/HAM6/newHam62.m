6f8c135d430a1c2c158937d1c71c957ea86d93be
Raffaele Terracino <raffaeleterracino001@gmail.com>
2022-11-23 11:07:03 +0100

    new files

--- Scripts/HAM6/newHam62.m
+++ Scripts/HAM6/newHam62.m
@@ -1,37 +1,37 @@
 function out = newHam62(img)
     %passaggio a 4096 colori
-    amiga = (round(img/17)*17); %quantizzazione a 4096 colori
+    quantImg = betterQuantN(img, 16);
     
-    [ind, map] = rgb2ind(amiga,16);
+    [ind, map] = rgb2ind(quantImg,16);
     map = round(map*255);
 
     out = zeros(size(img), 'uint8');
     [h, w] = size(img, [1 2]);
 
     k = ind(1, 1);
-    lastpx = [map(k+1, 1); map(k+1, 2); map(k+1, 3)];
+    prevpx = [map(k+1, 1); map(k+1, 2); map(k+1, 3)];
     start = 2;
-    out(1, 1, :) = lastpx;
+    out(1, 1, :) = prevpx;
 
-    amiga = double(amiga);
+    quantImg = double(quantImg);
 
     for i = 1:h
         for j = start:w
             %tecnica greedy: minimizzo l'errore localmente a ogni passo
             err = zeros([1 19]);
-            m = amiga(i, j, :);
+            m = quantImg(i, j, :);
             m = m(:)';
 
             err(1:16) = sqrt(sum((map - m).^2, 2));
 
             %mantengo verde e blu
-            err(17) = sqrt((amiga(i,j,2) -lastpx(2))^2 + (amiga(i,j,3) - lastpx(3))^2);
+            err(17) = sqrt((quantImg(i,j,2) -prevpx(2))^2 + (quantImg(i,j,3) - prevpx(3))^2);
             
             %mantengo rosso e blu
-            err(18) = sqrt((amiga(i,j,1) -lastpx(1))^2 + (amiga(i,j,3) - lastpx(3))^2);
+            err(18) = sqrt((quantImg(i,j,1) -prevpx(1))^2 + (quantImg(i,j,3) - prevpx(3))^2);
             
             %mantengo rosso e verde
-            err(19) = sqrt((amiga(i,j,1) -lastpx(1))^2 + (amiga(i,j,2) - lastpx(2))^2);
+            err(19) = sqrt((quantImg(i,j,1) -prevpx(1))^2 + (quantImg(i,j,2) - prevpx(2))^2);
             
             [~, p] = min(err);
 
@@ -39,17 +39,17 @@ function out = newHam62(img)
                 case num2cell(1:16)
                     out(i,j, :) = map(p, :);
                 case 17
-                    out(i, j, :) = [amiga(i,j, 1); lastpx(2); lastpx(3)];
+                    out(i, j, :) = [quantImg(i,j, 1); prevpx(2); prevpx(3)];
                 
                 case 18
-                    out(i, j, :) = [lastpx(1); amiga(i,j, 2); lastpx(3)];
+                    out(i, j, :) = [prevpx(1); quantImg(i,j, 2); prevpx(3)];
 
                 case 19
-                    out(i, j, :) = [lastpx(1); lastpx(2); amiga(i, j, 3)];
+                    out(i, j, :) = [prevpx(1); prevpx(2); quantImg(i, j, 3)];
           
             end
             k = ind(i, j);
-            lastpx = [map(k+1, 1); map(k+1, 2); map(k+1, 3)];
+            prevpx = [map(k+1, 1); map(k+1, 2); map(k+1, 3)];
         end
 
         start = 1;
