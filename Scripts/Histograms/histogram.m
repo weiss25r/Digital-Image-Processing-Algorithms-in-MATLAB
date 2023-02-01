01525a206ef829cd30249d0e48b1cef400095b43
Raffaele Terracino <raffaeleterracino001@gmail.com>
2023-02-01 17:04:07 +0100

    Update histogram.m

--- Scripts/Histograms/histogram.m
+++ Scripts/Histograms/histogram.m
@@ -1,10 +1,11 @@
 function h = histogram(img)
     h = imhist(uint8(img), 256)';
+    p = h(not(isoutlier(h)));
     %normalizzazione a 100, ceil per avere almeno 1 pixel nella barretta
-    m = 101-ceil(100*double(h)/double(max(h(:))));
+    m = 101-ceil(100*double(p)/double(max(p(:))));
     out = zeros(120, 256, 'uint8')+255;
 
-    for i = 1:256
+    for i = 1:numel(p)
         out(m(i):100, i) = 0;
         out(102:120, i) = i-1;
     end
